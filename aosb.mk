e9f1df19849cca53450049b801f83c6ed0ce401a
anerikg <anerikgames@gmail.com>
2014-02-14 19:56:07 -0200

    Update and rename cm.mk to aosb.mk

--- aosb.mk
+++ aosb.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aosb/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := dlx
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSB stuff.
+$(call inherit-product, vendor/aosb/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/dlx/device_dlx.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlx
-PRODUCT_NAME := cm_dlx
+PRODUCT_NAME := aosb_dlx
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := dlx
 PRODUCT_MANUFACTURER := htc
